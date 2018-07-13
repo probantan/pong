7d687041a42769dcb9286149e8862ed84de6b536
Protus <protus.bantan@gmail.com>
2018-07-13 18:57:01 +0300

    changed my background color for the button

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+//user interface
+$(document).ready(function(){
+    $('form#cup').submit(function(event) {
+      event.preventDefault();
+     
+      var input=parseInt($('input#btn').val());
+      var output=pingpong(input);
+console.log("pop");
+      
+    
+    })
+})
+
+var pingpong=function(input){
+    for(x=1; x<=input; x++){
+        if(x%15===0){
+            var li="<li>"+"pingpong"+"</li>";
+            //console.log('pingpong');
+        }else if (x%3===0) {
+            var li="<li>"+"ping"+"</li>";
+        }else if(x%5===0) {
+            var li="<li>"+"pong"+"</li>";
+
+        }else {
+            var li="<li>"+x+"</li>";
+        }
+$("#pop").append(li);
+    }
+}
